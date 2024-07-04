8ba87116f237e8a83053ba92c91e849ff74eb2bd
David Alvarez <david.alvarez321@hotmail.com>
2024-07-03 23:03:27 -0500

    cambio de carpeta

---
+++ Ejercicio_triangulo/apptriangulo.js
@@ -0,0 +1,7 @@
+function sumar() {
+  let num1 = document.getElementById('n1').value;
+  let num2 = document.getElementById('n2').value;
+  let suma = num1 + num2;
+  document.getElementById('resultado1').innerhtml = suma;
+  document.getElementById('resultado2').innerhtml = suma;
+}
---
+++ Iconos_ejercicio/ejemplo1.js
@@ -0,0 +1,10 @@
+let num1 = 1;
+let num2 = 5;
+let suma=num1+num2;
+console.log("la suma de los numeros es:",suma);
+
+let num3 = prompt("Ingrese numero1");
+let num4 = prompt("Ingrese numero2");
+let suma5 = parseFloat(num1) + parseFloat(num2);
+console.log("La suma es:",suma);
+alert("la suma no es"+suma);
