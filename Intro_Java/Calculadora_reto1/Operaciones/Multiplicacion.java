0b04898289068adc5dc1e92b9344ac33bce7d792
David Alvarez <david.alvarez321@hotmail.com>
2024-09-08 17:00:51 -0500

    algunos "Super" perdidos

--- Intro_Java/Calculadora_reto1/Operaciones/Multiplicacion.java
+++ Intro_Java/Calculadora_reto1/Operaciones/Multiplicacion.java
@@ -1,12 +1,10 @@
 package Calculadora_reto1.Operaciones;
 
-import Calculadora_reto1.Operaciones.Operaciones;
-
 public class Multiplicacion extends Operaciones {
     public Multiplicacion(float n1, float n2){
         super(n1, n2, 'x');
     }
     protected void operar() {
-        super.res = n1 * n2;
+        super.res = super.n1 * super.n2;
     }
 }
--- Intro_Java/Calculadora_reto1/Operaciones/Resta.java
+++ Intro_Java/Calculadora_reto1/Operaciones/Resta.java
@@ -1,7 +1,5 @@
 package Calculadora_reto1.Operaciones;
 
-import Calculadora_reto1.Operaciones.Operaciones;
-
 public class Resta extends Operaciones{
     public Resta(float n1, float n2){
         super(n1, n2, '-');
